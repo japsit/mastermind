22027e0570df338153b4dd2433026b2dbd8ed434
Juha Sarkkinen <juha.sarkkinen@japsit.fi>
2022-08-14 22:49:46 +0300

    Store final coding assignment

--- labra.c
+++ labra.c
@@ -18,6 +18,10 @@ void timer(void){
 	seconds++;
 }
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> fa53b3f (Store final coding assignment)
 // Funktio generoi s numeron sarjan
 int generatecode(char* code, int s)
 {
